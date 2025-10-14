190479704025d41d123433740aadc5f7dabf8438
Daniel Andrei R. Garcia <dannybuntu@proton.me>
2025-10-14 11:19:20 +0800

    updateVerifications.sh v0.0.1

---
+++ updateVerifications.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Check if GitHub Personal Access Token has been set
+#
+# updateVerifications.sh v0.0.1
+# An internal script by
+# Author: Daniel Andrei "Dannybuntu" R. Garcia
+# ============================================
+# Checks out to ws, updates ws, backs up verifications, then checks what needs to be verified.
+# Outputs file to home
+
+set -e # exit immediately if a command fails
+
+if [ -z "${GAP}" ]; then
+  echo "GAP is not set. Please export it first, eg:"
+  echo 'export GAP="your_github_personal_access_token"'
+  exit 1
+fi
+
+cd ~/work/walletScrutinyCom || exit
+git checkout master
+git fetch origin master
+git pull
+git checkout includeNostrBackuptoSearchVerifications
+node scripts/nostr/backupNostrVerificationEvents.mjs
+./refresh.sh -g "$GAP"
+node scripts/nostr/wsVerify.mjs --needs-verification | tee ~/needs-verification-$(date +%Y-%m-%d).txt
+git checkout master
+git clean -f 
+git reset --hard HEAD
